28aaea375888d2f0ebff7c0c317a2ba3013b7c82
Chaitali1982 <67077628+Chaitali1982@users.noreply.github.com>
2022-08-19 20:14:48 +0100

    Update about.php

--- 33-main/33-main/about.php
+++ 33-main/33-main/about.php
@@ -76,7 +76,7 @@ body {
 
         <div class="section-title">
           <h2>About</h2>
-          <p>A bright, talented, ambitious, and self-motivated web designer with a strong technical background. My mission is to design and develop the best website around.</p>
+          <p>A bright, talented, ambitious, and self-motivated software tester technical background. My mission is to design and test the best website around.</p>
         </div>
 
         <div class="row">
@@ -84,7 +84,7 @@ body {
           
           </div>
           <div class="col-lg-8 pt-4 pt-lg-0 content">
-            <h3>Front End Devloper &amp; Back End Devloper </h3>
+            <h3> Software tester &amp;Front End Devloper, Back End Devloper </h3>
            </br>
             <div class="row">
               <div class="col-lg-6">
@@ -121,21 +121,21 @@ body {
           <div class="col-lg-6">
 
             <div class="progress">
-              <span class="skill">php <i class="val">100%</i></span>
+              <span class="skill">Maual testing <i class="val">100%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
             <div class="progress">
-              <span class="skill">CSS <i class="val">90%</i></span>
+              <span class="skill">Automation testing <i class="val">90%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
 
             <div class="progress">
-              <span class="skill">JavaScript <i class="val">75%</i></span>
+              <span class="skill">GITHUB<i class="val">75%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
@@ -146,12 +146,17 @@ body {
           <div class="col-lg-6">
 
             <div class="progress">
-              <span class="skill">PHP <i class="val">80%</i></span>
+              <span class="skill">Database,Mobile testing <i class="val">80%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
-
+ <div class="progress">
+              <span class="skill">POSTMAN <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
             <div class="progress">
               <span class="skill">MYSQL <i class="val">80%</i></span>
               <div class="progress-bar-wrap">
@@ -160,13 +165,19 @@ body {
             </div>
 
             <div class="progress">
-              <span class="skill">Python <i class="val">55%</i></span>
+              <span class="skill">Selenium <i class="val">55%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
             <div class="progress">
-              <span class="skill">Java <i class="val">55%</i></span>
+              <span class="skill">Java<i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+             <div class="progress">
+              <span class="skill">Cucumber<i class="val">55%</i></span>
               <div class="progress-bar-wrap">
                 <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
               </div>
